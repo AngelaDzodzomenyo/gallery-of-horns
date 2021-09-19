e8c23cf40e5ddc47f5edf571f7868557c6eb37a6
Angela Dzodzomenyo <enyodzo@gmail.com>
2021-09-18 20:54:25 -0500

    created new files

---
+++ src/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class Header extends React.Component {
+  render(){
+    return(
+      <h1>Aliens was better than the original</h1>
+    )
+  }
+
+}
+
+export default Header;
---
+++ src/Main.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Main extends React.Component {
+  render(){
+    return(
+      <>
+      <h2>Horned Beasts and Where To Find Them</h2>
+      </>
+    )
+  }
+
+}
+
+export default Main;
