3cb89d2a9282c13c430d96dbe8b6a179b8328c95
Angela Dzodzomenyo <enyodzo@gmail.com>
2021-09-20 00:00:50 -0500

    added images

--- src/HornedBeasts.js
+++ src/HornedBeasts.js
@@ -4,8 +4,11 @@ class HornedBeasts extends React.Component {
   render(){
     return(
       <>
+        <h2 className="title">{this.props.title}</h2>
+        <img src={this.props.imageURL} alt="Picture of Horned Beasts"/>
+        <p className="description">{this.props.description}</p>
       </>
-    )
+    );
   }
 
 }
