f1e1d3670bd9a6f32bf6d1a85fff4604875f5d32
Angela Dzodzomenyo <enyodzo@gmail.com>
2021-09-22 23:59:10 -0500

    added json

--- src/HornedBeasts.js
+++ src/HornedBeasts.js
@@ -1,12 +1,34 @@
 import React from 'react';
+// import Button from 'react-bootstrap/Button'
+// import Card from 'react-bootstrap/Card'
+
 
 class HornedBeasts extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      clickOnBeast: 0,
+      isFav: false
+    };
+  }
+
+
+
+  onLike =() => {
+    console.log('add button pressed');
+    this.setState({ clickOnBeast: this.state.clickOnBeast + 1, isFav: true});
+  }
+  
+
   render(){
     return(
       <>
         <h2 className="title">{this.props.title}</h2>
-        <img src={this.props.imageURL} alt="Picture of Horned Beasts"/>
+        <img src={this.props.image_url} alt="A Horned Beast"/>
         <p className="description">{this.props.description}</p>
+        <p className="horns">{this.props.horns}</p>
+        <p>Amount of Times Favorited: {this.state.clickOnBeast} {(this.state.isFav) ? '❤️' : ''}</p>
+        <button onClick={this.onLike}>Like</button>
       </>
     );
   }
@@ -14,3 +36,6 @@ class HornedBeasts extends React.Component {
 }
 
 export default HornedBeasts;
+
+
+
--- src/Main.js
+++ src/Main.js
@@ -1,13 +1,26 @@
 import React from 'react';
 import HornedBeasts from './HornedBeasts';
+import data from './data.json'
 
 class Main extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+    }
+  }
   render(){
-    return(
+    let hornedarr = [];
+
+    data.forEach((horned,idx) => {
+      hornedarr.push(
+        <HornedBeasts key={idx} title={horned.title} image_url={horned.image_url} description={horned.description} />
+      );
+    })
+
+    return (
       <>
       <h2>Horned Beasts and Where To Find Them</h2>
-      <HornedBeasts title='TITLE1' imageURL ='https://images.unsplash.com/photo-1470688090067-6d429c0b2600?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80'/>
-      <HornedBeasts title='TITLE2' imageURL ='https://images.unsplash.com/photo-1535082427395-4a20c7c7af88?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80'/>
+      {hornedarr}
       </>
     );
   }
@@ -15,3 +28,7 @@ class Main extends React.Component {
 }
 
 export default Main;
+
+
+/* <HornedBeasts title='TITLE1' imageURL ='https://images.unsplash.com/photo-1470688090067-6d429c0b2600?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80'/>
+<HornedBeasts title='TITLE2' imageURL ='https://images.unsplash.com/photo-1535082427395-4a20c7c7af88?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80'/> */
